bd4590874c037499ce2e501b4f6f977d0df0dc44
Eren <mrdagdemir@gmail.com>
2023-08-18 15:55:16 -0400

    first commit

---
+++ Jenkins/main.java
@@ -0,0 +1,10 @@
+package Jenkins;
+
+public class main {
+    public static void main(String[] args) {
+
+        for(int i = 0; i<5; i++){
+            System.out.println("Eren is studying");
+        }
+    }
+}
